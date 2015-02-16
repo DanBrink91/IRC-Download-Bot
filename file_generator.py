025d82710228a1f3d7988c7fe3b159472d95d41e
Elizabeth Williams <elizabeth.williams@ucf.edu>
2015-02-15 22:47:13 -0500

    Added alternative titles, making json instead of txt files

--- file_generator.py
+++ file_generator.py
@@ -1,36 +1,59 @@
 import requests
 import myanimelist.session
 import mal_settings
+import json
+#PECKLE
+import jsonpickle
 
 # Populate file with current 'watching' list on MAL
+
+class Entry:
+	def __init__(self, titles, episode):
+		self.titles = titles
+		self.episode = episode
 
 def main():
 	
 	# get watching list and episodes
 	session = myanimelist.session.Session(username=mal_settings.username, password=mal_settings.password)
 	session.login()
-	test = myanimelist.anime_list.AnimeList(session, mal_settings.user)
-	print test.list
+	user_list = myanimelist.anime_list.AnimeList(session, mal_settings.user)
+
 	watching = {}
-	for key, value in test.list.items():
+	for key, value in user_list.list.items():
+
 		if value['status'] == "Watching":
-			watching[key.title] = value['episodes_watched']
-	print watching
+
+			titles = []
+			titles.append(key.title)
+			# Get the alternative titles and add them
+			for alt_title, val in key.alternative_titles.iteritems():
+
+				if alt_title == "English" or alt_title == "Synonyms":
+
+					# Sometimes it is a list of alternative titles..
+					if type(val) is list:
+						for title in val:
+							titles.append(title)
+					else:
+						titles.append(val)
+						
+			watching[key] = Entry(titles, value['episodes_watched'])
+
 
 	# populate file
 	# 'w' will truncate existing file
 	# 'a' will not
-	filename = "watching_list.txt"
-	line = ""
-	try: 
+
+	filename = "watching_list.json"
+
+	try:
 		file_content = open(filename, 'w')
-		for anime, episode in watching.items():
-			# encode because of titles like Tokyo Ghoul square root A
-			line = str(anime.encode('utf8')) + " - " + str(episode) + '\n' 
-			file_content.write(line)
+		json_list = jsonpickle.encode(watching)
+		file_content.write(json_list)
 		file_content.close()
 	except:
-		print "Error writing to txt file"
+		"Error writing json file"
 
 
 if __name__ == "__main__":
