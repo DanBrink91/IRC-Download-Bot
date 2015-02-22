513db257e7a88f45d8b7cb66410396d0beccfbb2
Dan Brinkman <danbrink91@gmail.com>
2015-02-22 00:19:35 -0500

    added web interface

---
+++ web/app.py
@@ -0,0 +1,71 @@
+import sqlite3
+from flask import Flask, g, render_template
+
+DATABASE = "../mal_db.db"
+app = Flask(__name__)
+
+def make_dicts(cursor, row):
+	"""
+	Lets us get a dictionary from sqlite instead of a tuple
+	"""
+	return dict((cursor.description[idx][0], value)
+	                for idx, value in enumerate(row))
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+		db.row_factory = make_dicts
+	return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
+
+def query_db(query, args=(), one=False):
+	"""
+	helper function for db queries
+	"""
+	cur = get_db().execute(query, args)
+	rv = cur.fetchall()
+	cur.close()
+	return (rv[0] if rv else None) if one else rv
+# =====================================================
+# Views
+# =====================================================
+
+@app.route('/')
+def index():
+	ongoing_anime = query_db("""SELECT  animes.id, animes.title,  MIN(episodes.number) AS current_episode, episodes.id AS episode_id FROM animes
+JOIN episodes ON episodes.series = animes.id
+WHERE animes.max_ep > (SELECT IFNULL(MAX(episodes.number), animes.current_ep)  FROM episodes 
+WHERE  episodes.series=animes.id AND episodes.status = 1)
+AND episodes.status = 0
+GROUP BY animes.id""")
+	
+	completed_anime = query_db("""SELECT  animes.id, animes.title,  MIN(episodes.number) AS current_episode, episodes.id AS episode_id FROM animes
+JOIN episodes ON episodes.series = animes.id
+WHERE animes.max_ep = (SELECT IFNULL(MAX(episodes.number), animes.current_ep)  FROM episodes 
+WHERE  episodes.series=animes.id AND episodes.status = 1)
+AND episodes.status = 0
+GROUP BY animes.id""")
+	
+	return render_template('index.html', ongoing=ongoing_anime, completed=completed_anime)
+
+@app.route('/anime/<int:anime_id>')
+def anime(anime_id):
+	anime_info = query_db("SELECT * FROM animes WHERE id=?", (anime_id, ), True)
+	episodes = query_db("SELECT * FROM episodes WHERE series=?", (anime_id, ))
+
+	return render_template('anime.html', info=anime_info, episodes=episodes)
+
+@app.route('/episode/<int:episode_id>')
+def episode(episode_id):
+	episode_info = query_db("SELECT * FROM episodes WHERE id=?", (episode_id, ), True)
+	anime_info = query_db("SELECT * FROM animes WHERE id=?", (episode_info['series'], ), True)
+
+	return render_template('episode.html', episode=episode_info, anime=anime_info)
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
